28509c91a796b01fa5f92760b2d06055ca215d58
unknown <islammohamedahmed49@gmail.com>
2024-03-16 18:49:37 +0200

    suffix three

---
+++ SuffixThree/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+int t;
+string[] str1 = new string[35];
+
+t = ToInt32(ReadLine());
+for (int i = 0; i < t; i++)
+{
+    string str2 = ReadLine();
+    char ch = str2[str2.Length - 1];
+    switch (ch)
+    {
+        case 'o':
+            str1[i] = "FILIPINO";
+            break;
+        case 'u':
+            str1[i] = "JAPANESE";
+            break;
+        case 'a':
+            str1[i] = "KOREAN";
+            break;
+    }
+}
+for (int i = 0; i < t; i++)
+{
+    WriteLine(str1[i]);
+}
